d295254ec479b51efac599b5a6da1259fea93e28
Sneha342 <snehan342@gmail.com>
2023-04-06 12:50:30 +0530

    login edit

--- doctor-desktop-app/Doctor-Desktop-Application/WpfApp1/obj/Debug/net6.0-windows/Page1.g.i.cs
+++ doctor-desktop-app/Doctor-Desktop-Application/WpfApp1/obj/Debug/net6.0-windows/Page1.g.i.cs
@@ -53,7 +53,7 @@ namespace DoctorAppointments {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/DoctorAppointments;V1.0.0.0;component/page1.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/DoctorAppointments;component/page1.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Page1.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
